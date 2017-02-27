15c8db2cb9e6e6d620f2288f2fb0c59dfcbc24fb
ycwei982 <ycwei982@gmail.com>
2017-02-27 15:29:52 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+import urllib2
+import json
+word = raw_input("Type the world you want the pronunciation: ")
+html = urllib2.urlopen(r'http://fanyi.youdao.com/openapi.do?keyfrom=WrongPronounce&key=602608676&type=data&doctype=json&version=1.1&q=%s'%word)
+jsonfetch = json.loads(html.read())
+print(jsonfetch)
+print("us:"+jsonfetch['us-phonetic'])
